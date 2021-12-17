bc4827cd4a6b37fe5d4eeba4131ebcf7f99e61fd
walrus811 <walrusray811@gmail.com>
2021-12-17 23:56:49 +0900

    who

--- src/pages/episode/{SanityEpisode.slug__current}.js
+++ src/pages/episode/{SanityEpisode.slug__current}.js
@@ -26,10 +26,12 @@ export default function SanityEpisode({ data }) {
   const episode = data.sanityEpisode;
   return (
     <Layout title={episode.title} description={episode.description}>
-      <GatsbyImage
-        image={episode.image.asset.gatsbyImageData}
-        alt={episode.title}
-      ></GatsbyImage>
+      {episode.image?.asset?.gatsbyImageData && (
+        <GatsbyImage
+          image={episode.image.asset.gatsbyImageData}
+          alt={episode.title}
+        ></GatsbyImage>
+      )}
       <h1>{episode.title}</h1>
       <p>
         (posted {episode.date}) - {episode.description}
